684418e9ca37a30c6b236a234926cf003b0e7d2e
Sthéphany Karoline Soares de Araújo Tezza <sthearaujo9@gmail.com>
2021-11-05 11:54:56 -0300

    Upgrade version to 2.10.8

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Center for Recognition and Inspection of Cells (CRIC)'
 author = 'Center for Recognition and Inspection of Cells (CRIC)'
 
 # The full version, including alpha/beta/rc tags
-release = '2.10.7'
+release = '2.10.8'
 
 
 # -- General configuration ---------------------------------------------------
